b2ab8a012152f97578a0f5ffb956d82f8516a7d2
rogerelooney <123662949+rogerelooney@users.noreply.github.com>
2023-03-07 22:13:37 +0000

    Project Update 1.1

--- Projects/Connect4.h
+++ Projects/Connect4.h
@@ -8,19 +8,31 @@
 #include <iomanip>
 #include <cmath>
 
+using namespace std;
+
 class Connect4
 {
     private:
     string players[2];
+    string grid[6][7];
     
     Connect4()
     {
         players[2] = {"X", "O"};
+        grid[6][7] = {
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "}
+        };
     }
 
     Connect4(const Connect4& obj)
     {
         players[2] = obj.players[2];
+        grid[6][7] = obj.grid[6][7];
     }
 
     Connect4& operator=(const Connect4& rhs)
@@ -28,6 +40,7 @@ class Connect4
         if(this != rhs)
         {
             players[2] = rhs.players[2];
+            grid[6][7] = rhs.grid[6][7];
         }
         return *this;
     }
@@ -42,12 +55,20 @@ class Connect4
 
     bool MakeMove()
     {
+        int x;
 
     }
 
     void Reset()
     {
-
+        grid[6][7] = {
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", " ", " ", " ", " ", " ", " "}
+        };
     }
 
     const char CurrentPlayer()
@@ -57,8 +78,20 @@ class Connect4
 
     const string ToString()
     {
-
+        cout << "   0   1   2   3   4   5   6 \n"; // Top of grid
+        
+        cout << "5 ";
+        for(int i = 0; i < 8; i += 1)
+        {
+            cout << "[" + grid[0][i] + "] ";
+        }
     }
+
+    friend ostream& operator<<(ostream& out, const Connect4& obj)
+        {
+            out << obj.ToString();
+            return out;
+        }
 };
 
 #endif
