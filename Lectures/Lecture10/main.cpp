cf2943ef3a8829ea15e3cd612924385508ad8e9f
rogerelooney <123662949+rogerelooney@users.noreply.github.com>
2023-05-02 17:40:33 +0000

    Lectures Update

---
+++ Lectures/Lecture10/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "Queue.h"
+
+int main()
+{
+    Queue<int> q;
+
+    for(int i = 100; i <= 200; i += 20)
+    {
+        q.Enqueue(i);
+        std::cout << i << " ";
+    }
+    std::cout << "\n";
+
+    while(!q.IsEmpty())
+    {
+        std::cout << q.Peek() << " ";
+        q.Dequeue();
+    }
+    std::cout << "\n";
+    return 0;
+}
